2b5517ea24b2bae1512517ec2f5e5ba64203d83e
Minal <mnlptl84@gmail.com>
2023-09-13 17:22:05 +0530

    adde new feature

---
+++ src/test/java/com/crm/qa/testcases/DiscountPageTest.java
@@ -0,0 +1,15 @@
+package com.crm.qa.testcases;
+
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+
+public class DiscountPageTest {
+
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public DiscountPageTest()
+	{
+		super();
+	}
+}
